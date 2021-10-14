269e23f20b53a6182cf40008c475af2c61511df3
Miheretu Teshale <se.miheretu.degebassa@gmail.com>
2021-10-14 17:22:52 +0300

    creating new post, updating, deleting post is working fine

--- controllers/posts.js
+++ controllers/posts.js
@@ -34,7 +34,8 @@ exports.getPost = asyncHandler(async (req, res, next) => {
 //@access private
 
 exports.createPost = asyncHandler(async (req, res, next) => {
-  res.status(200).json({ status: 200, data: `post a new post` });
+  const post = await Post.create(req.body);
+  res.status(201).json({ success: true, data: post });
 });
 
 //@desc PUT a post
@@ -42,9 +43,20 @@ exports.createPost = asyncHandler(async (req, res, next) => {
 //@access private
 
 exports.updatePost = asyncHandler(async (req, res, next) => {
+  const post = await Post.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  if (!post) {
+    return next(
+      new ErrorResponse(`Post With Id ${req.params.id} not found`, 404)
+    );
+  }
+
   res.status(200).json({
     status: 200,
-    data: `Update a specific post whit and Id of ${req.params.id}`,
+    data: post,
   });
 });
 
@@ -53,8 +65,13 @@ exports.updatePost = asyncHandler(async (req, res, next) => {
 //@access public
 
 exports.deletePost = asyncHandler(async (req, res, next) => {
+  const post = await Post.findByIdAndDelete(req.params.id);
+
+  if(!post){
+      return next(new ErrorResponse(`Post with id ${req.params.id} not found`, 404))
+  }
   res.status(200).json({
-    status: 200,
-    data: `Delete a specific post whit and Id of ${req.params.id}`,
+    success: true,
+    data: {},
   });
 });
