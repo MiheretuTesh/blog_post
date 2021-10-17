65ce0a2de82a9e96136216c898e52e6020d96c80
Miheretu Teshale <se.miheretu.degebassa@gmail.com>
2021-10-17 08:31:10 +0300

    review model is created.

---
+++ models/Review.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const ReviewSchema = new mongoose.Schema({
+  comment: {
+    type: String,
+    required: [true, "please provide post title"],
+    maxlength: [100, "comment title cannot be more than 100 charachters"],
+  },
+  isLiked: {
+    type: boolean,
+    defaudlt: false,
+  },
+  post_id: {
+    type: mongoose.Schema.ObjectId,
+    ref: "Post",
+    required: true,
+  },
+  user_id: {
+    type: mongoose.Schema.ObjectId,
+    ref: "User",
+    required: true,
+  },
+});
+
+const Review = mongoose.model("Review", ReviewSchema);
+
+module.exports = Review;Q
